da49274297c8d7ff007f98348a82d65f209065fc
ramziddinolimov <ramizmafiya@gmail.com>
2023-04-28 00:22:02 +0500

    show

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const menuItems = document.querySelectorAll(".submenu-item")
 const submenuTitles = document.querySelectorAll(".submenu .menu-title")
 
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        item.classList.add("show-submenu")
+        menuItems.forEach((item2, index2) => {
+            if(index !== index2) {
+                item2.classList.remove("show-submenu")
+            }
+        })
+    })
+})
+
 console.log(menuItems, submenuTitles);
